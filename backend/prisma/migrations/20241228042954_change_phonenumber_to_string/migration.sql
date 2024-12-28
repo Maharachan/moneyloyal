4d08fc754d63cd8dda4b68e6bf53b259a8c160dc
codewithbalaji <balaji01975@gmail.com>
2024-12-28 14:07:03 +0530

    feat: implement user authentication login and signup api

---
+++ backend/prisma/migrations/20241228042954_change_phonenumber_to_string/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `users` MODIFY `phonenumber` VARCHAR(191) NOT NULL;
---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express, {Express, Request, Response } from "express";
+import { PORT } from "./secrets";
+import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
+import { errorMiddleware } from "./middlewares/error";
+import { signupSchema } from "./schema/user";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use('/api', rootRouter);
+
+
+export const prismaClient = new PrismaClient({
+    log:['query']
+})
+
+app.use(errorMiddleware);
+
+
+
+
+app.listen(PORT, () => {
+    console.log("app listening on port " + PORT + "!");
+});
